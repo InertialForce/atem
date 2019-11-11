e2000f73983ed6b5404b142bf1ac730f756820cb
InertialForce <InertialForce@users.noreply.github.com>
2019-11-11 22:22:24 +0300

    refactor, oop, store

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,75 +1,75 @@
-let rerenderEntireTree = () => {
-
-};
-
-let state = {
-  profilePage: {
-    postsData: [
-      {id: 1, message: "Hello", likes: 23},
-      {id: 2, message: "How are you?", likes: 3},
-      {id: 3, message: "Hey", likes: 42},
-    ],
-    newPostText: ""
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        {id: 1, message: "Hello", likes: 23},
+        {id: 2, message: "How are you?", likes: 3},
+        {id: 3, message: "Hey", likes: 42},
+      ],
+      newPostText: ""
+    },
+    messagePage: {
+      dialogsData: [
+        {id: 1, name: "Kirill"},
+        {id: 2, name: "Yuri"},
+        {id: 3, name: "Nadya"},
+        {id: 4, name: "Alex"},
+        {id: 5, name: "Max"},
+        {id: 6, name: "Petr"},
+        {id: 7, name: "Viki"}
+      ],
+      messagesData: [
+        {id: 1, message: "Hi"},
+        {id: 2, message: "How"},
+        {id: 3, message: "Took"},
+        {id: 4, message: "Now"},
+        {id: 5, message: "Root"}
+      ],
+      newMessageText: ""
+    },
+    friendsPage: {
+      friendData: [
+        {name: "Kirill"},
+        {name: "Viki"},
+        {name: "Petr"},
+      ]
+    }
   },
-  messagePage: {
-    dialogsData: [
-      {id: 1, name: "Kirill"},
-      {id: 2, name: "Yuri"},
-      {id: 3, name: "Nadya"},
-      {id: 4, name: "Alex"},
-      {id: 5, name: "Max"},
-      {id: 6, name: "Petr"},
-      {id: 7, name: "Viki"}
-    ],
-    messagesData: [
-      {id: 1, message: "Hi"},
-      {id: 2, message: "How"},
-      {id: 3, message: "Took"},
-      {id: 4, message: "Now"},
-      {id: 5, message: "Root"}
-    ],
-    newMessageText: ""
+  getState() {
+    return this._state;
   },
-  friendsPage: {
-    friendData: [
-      {name: "Kirill"},
-      {name: "Viki"},
-      {name: "Petr"},
-    ]
-  }
+  _callSubscriber() {},
+  addPost() {
+    debugger;
+    let newPost = {
+      id: 4,
+      message: this._state.profilePage.newPostText,
+      likes: 0
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._callSubscriber(this._state)
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state)
+  },
+  addMessage() {
+    let newMessage = {
+      id: 6,
+      message: this._state.messagePage.newMessageText
+    };
+    this._state.messagePage.messagesData.push(newMessage);
+    this._state.messagePage.newMessageText = "";
+    this._callSubscriber(this._state)
+  },
+  updateNewMessageText(newText) {
+    this._state.messagePage.newMessageText = newText;
+    this._callSubscriber(this._state)
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
 };
 
-export const addPost = () => {
-  let newPost = {
-    id: 4,
-    message: state.profilePage.newPostText,
-    likes: 0
-  };
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state)
-};
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state)
-};
-
-export const addMessage = () => {
-  let newMessage = {
-    id: 6,
-    message: state.messagePage.newMessageText
-  };
-  state.messagePage.messagesData.push(newMessage);
-  state.messagePage.newMessageText = "";
-  rerenderEntireTree(state)
-};
-export const updateNewMessageText = (newText) => {
-  state.messagePage.newMessageText = newText;
-  rerenderEntireTree(state)
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
